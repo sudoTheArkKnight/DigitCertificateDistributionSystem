8698ce44b0e0f71e17ba92e3c43e1a29fea05ad4
Abhishek Kumar <Abhishekkumaronroute@gmail.com>
2024-05-03 13:16:15 +0530

    fixing redirection

--- api/checking.js
+++ api/checking.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res) => {
   const rollno = req.query.rollno;
-  const pdfUrl = `https://InoraLabs.github.com/certificates/${rollno}.pdf`;
+  const pdfUrl = `https://your-github-username.github.io/certificates/${rollno}.pdf`;
   res.redirect(302, pdfUrl);
 };
